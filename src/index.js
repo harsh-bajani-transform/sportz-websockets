958d6ddf9ef4dc1dd7aa1d9dad9856c21bdb84f3
harsh-bajani-transform <harsh.bajani@transformsolution.net>
2026-02-23 15:49:07 +0530

    project setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+const port = 8001;
+
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send('Hello from the express server!');
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port http://localhost:${port}`);
+});
