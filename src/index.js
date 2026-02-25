8eef5150a6dd3a6ef29ba2a7d34abf87f48b9883
harsh-bajani-transform <harsh.bajani@transformsolution.net>
2026-02-25 12:01:28 +0530

    implement arcjet

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { matchRouter } from "./routes/matches.js";
 import http from "http";
 import { attachWebSocketServer } from "./ws/server.js";
+import { securityMiddleware } from "./arcjet.js";
 
 const app = express();
 const PORT = Number(process.env.PORT || 8001);
@@ -13,6 +14,8 @@ const server = http.createServer(app);
 app.get("/", (req, res) => {
   res.send("Hello from the express server!");
 });
+
+app.use(securityMiddleware());
 
 app.use("/matches", matchRouter);
 
